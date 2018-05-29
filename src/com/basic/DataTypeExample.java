84aabde5b43477806906928bcf6a7df4a883c2d1
gowthamkris <gowthamrajavel07@gmail.com>
2018-05-29 15:33:52 +0530

    DataTypeExample

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,9 @@
+package com.basic;
+
+public class DataTypeExample {
+public static void main(String arg[]) {
+	String firstName="gowtham";
+	String lastName="karthik";
+	System.out.println(firstName+""+lastName);
+}
+}
