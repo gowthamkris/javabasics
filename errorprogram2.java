be56b3deacb6d6c272f8e1c0bbfef6899d0ee211
gowthamkris <30409106+gowthamkris@users.noreply.github.com>
2018-05-29 21:51:39 +0530

    Create errorprogram2.java

---
+++ errorprogram2.java
@@ -0,0 +1,20 @@
+for ( int i = 1; i <= 10; i++ )
+this loop repeat itration 10 times
+            for ( int j = 1; j <= 5; j++ )
+this loop print 5 continous '@' symbol
+
+
+
+Output
+------
+
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
