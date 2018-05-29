0d306b722d1647bcce87ee1534b88e4a8e85007b
gowthamkris <gowthamrajavel07@gmail.com>
2018-05-29 17:44:53 +0530

    condition

---
+++ src/basic/DataType.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class DataType {
+public static void main(String arg[]) {
+	int a=10,b=20;
+	if(a<b)
+	{
+		System.out.println(b+"is greater than"+a);
+	}
+	else
+	{
+		System.out.println(a+"is less than"+b);
+	}
+}
+}
