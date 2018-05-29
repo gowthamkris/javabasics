36205a0aaa3752e812f3844abb154eb5b95effac
gowthamkris <gowthamrajavel07@gmail.com>
2018-05-29 17:48:32 +0530

    switch condition

---
+++ src/case1/operators.java
@@ -0,0 +1,20 @@
+package case1;
+
+public class operators {
+	public static void main(String arg[]) {
+		int a=8;
+		switch(a)
+		{
+		case 1:System.out.println("man");
+		break;
+		case 2:System.out.println("men");
+		break;
+		case 8:System.out.println("child");
+		break;
+		default:System.out.println("default");
+		break;
+		
+		}
+	}
+
+}
