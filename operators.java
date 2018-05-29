dd82b45bca38b40152c965e6daaf5094f59e08da
gowthamkris <30409106+gowthamkris@users.noreply.github.com>
2018-05-29 22:12:37 +0530

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,27 @@
+public class Op1 {
+	
+
+	public static void main(String s[]) {
+		String myName="gowsik";
+	int a=10;
+	int b;
+	a++;
+	b=a;
+	b = b + a;
+	b *=a;
+System.out.println("the value is "+b+"");
+boolean x=true; 
+if(x!=false) {
+	System.out.println(x);
+	System.out.println("my name is"+myName+" and my token number is "+a+"");
+	System.out.println(a>b);
+	System.out.print(b>>2);
+}
+	
+
+	}
+}
+
+
+	}
+}
