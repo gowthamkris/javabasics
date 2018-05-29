793ebf70dc5164f43b1f174e6c58896a482148ae
gowthamkris <gowthamrajavel07@gmail.com>
2018-05-29 21:08:55 +0530

    operators

---
+++ src/basic/element.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class element {
+	public static void main(String s[]) {
+		String myName="gowtham";
+	int a=10;
+	int b;
+	a++;
+	b=a;
+	b = b + a;
+	b *=a;
+System.out.println("the value is "+b+"");
+boolean x=true; 
+if(x!=false) {
+	System.out.println(x);
+	System.out.println("my name is"+myName+" and my token number is "+a+"");
+	System.out.println(a>b);
+	System.out.print(b>>2);
+}
+	}
+}
+
+
