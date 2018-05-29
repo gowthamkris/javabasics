91c7bba158355781d358f077f368327ec0c71e03
gowthamkris <30409106+gowthamkris@users.noreply.github.com>
2018-05-29 22:17:18 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,19 @@
+public class Switch{
+	public static void main(String s[]) {
+		int a=15;
+		switch(age) {
+		case 10:
+			System.out.println("child");
+			break;
+		case 18:
+			System.out.println("Adult");
+			break;
+		case 15:
+			System.out.println("old");
+			break;
+		 default:
+			 System.out.println("default");
+	break;
+	}
+  }
+  }
