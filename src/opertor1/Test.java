daf8f4d2370734e071919a56024189cf8f83834b
gowthamkris <gowthamrajavel07@gmail.com>
2018-05-30 12:19:38 +0530

    String

---
+++ src/opertor1/Test.java
@@ -0,0 +1,11 @@
+package opertor1;
+
+public class Test {
+public static void main(String[] args)
+{
+	String s="TWO"+2*4+"THREE"+5+6+"FOUR"+"FIVE";
+	System.out.println(s);
+	int i=19+ +52- -26+ +16;
+	System.out.println(i);
+}
+}
