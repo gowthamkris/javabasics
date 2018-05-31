c953b669bf63effd88141a5184a97a9c3318e7d9
gowthamkris <gowthamrajavel07@gmail.com>
2018-05-31 12:56:32 +0530

    DateMonth1

--- src/opertor1/DateMonth.java
+++ src/opertor1/DateMonth.java
@@ -3,6 +3,16 @@ package opertor1;
 public class DateMonth {
 int Day1;int month1;
 int year;
+public DateMonth()
+{
+	
+}
+public DateMonth(int Day1,int month1,int year)
+{
+	this.Day1=Day1;
+	this.month1=month1;
+	this.year=year;
+}
 public int getDay1() {
 	return Day1;
 	
@@ -24,16 +34,21 @@ public int getyear()
 public void setyear(int year)
 {
 	this.year=year;
-	System.out.println(year);
+	//System.out.println(year);
 }
-public void displayDateMonth(int Day1,int month1,int year)
+public void displayDateMonth()
 {
 	System.out.println("Date is "+Day1+"/"+month1+"/"+year+"");
 	
 	}
 public static void main(String args[]) {
 	DateMonth dm=new DateMonth();
-	dm.displayDateMonth(30,11,1997);
-	dm.displayDateMonth(15, 03, 1997);
+	dm.setDay1(31);
+	dm.setmonth1(11);
+	dm.setyear(1997);
+	dm.displayDateMonth();
+	//dm.displayDateMonth(30,11,1997);
+	
+	
 }
 }
