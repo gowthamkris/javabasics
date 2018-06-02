75bdc4774384088530096b4a9cfcb07ac6e631cc
gowthamkris <gowthamrajavel07@gmail.com>
2018-06-02 11:43:30 +0530

    Palindrom

--- java/src/basic/Palindrom.java
+++ java/src/basic/Palindrom.java
@@ -1,21 +1,5 @@
 package basic;
 
 public class Palindrom {
-public static void main(String args[])
-{
-	int N=9876;
-	int t=N;
-	int arr[]=new int[10];
-	int i=0;
-	while(t>0)
-	{
-		int remainder=t%10;
-		arr[i]=remainder;
-		i++;
-		t=t/10;
-	}
-	for(i=0;i<10;i++) {
-	System.out.println(arr[i]);
-	}
-}
+
 }
